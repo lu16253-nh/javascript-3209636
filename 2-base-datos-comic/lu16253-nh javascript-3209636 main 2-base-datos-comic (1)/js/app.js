48a5c6ee2c8e8a3d80913d59b673e2167d33a7bc
Luisa Restrepo <restrepocarvajalluisaf100@gmail.com>
2025-11-21 15:27:06 -0500

    Add files via upload

---
+++ 2-base-datos-comic/lu16253-nh javascript-3209636 main 2-base-datos-comic (1)/js/app.js
@@ -0,0 +1,19 @@
+const seccionPrincipal = document.querySelector(".seccion-principal")
+const contenidoPrincipal = document.querySelector(".contenido-principal")
+
+console.log(seccion-principal)
+console.log(contenido-principal)
+
+seccionPrincipal.innerHTML= `
+ <div class="contenido-principal">
+        <h1>${nombreComic}<h1>
+        <p>${sinopsis}</p>
+        <a class="boton" href="#">Ver el Cómic</a>
+      </div>
+      <div class="imagen-principal" style="background-image:url('img/imagen.jpg')"
+`
+comic.escenas.forEach8(escenas => {
+  const miCard = document.querySelector
+}
+
+)
