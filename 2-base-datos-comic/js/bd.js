4bd60a2fd9858db467fabf451d072ac023828d28
Luisa Restrepo <restrepocarvajalluisaf100@gmail.com>
2025-12-09 15:22:10 -0500

    Actualización

---
+++ 2-base-datos-comic/js/bd.js
@@ -0,0 +1,29 @@
+const comics = [
+  {
+    libertad: "",
+    year: 2025,
+    genero: ["Ciencia ficción"],
+    personajes: ["Leo", "Nora", "Ninove"],
+    portada: "./images/portada.jpg",
+    escenas: [
+      {
+        id: "1",
+        nombre: "Una sola mente colectiva",
+        personajes: ["Leo", "Nora", "Ninove"],
+        imagen: "./images/portada.jpg"
+      },
+      {
+        nombre: "Leo",
+        imagen: "./img/leo.jpg"
+      },
+      {
+        nombre: "Nora",
+        imagen: "./img/nora.jpg"
+      },
+      {
+        nombre: "Ninove",
+        imagen: "./img/ninove.jpg"
+      }
+    ]
+  }
+];
